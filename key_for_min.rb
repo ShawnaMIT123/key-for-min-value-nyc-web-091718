aa018ee1fa83ddc2dfe668dcb0d351e78b9dd968
Shawna Davis <shawna_davis@alum.mit.edu>
2018-09-13 19:03:47 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  valuemin = name_hash[0]
+  valuemin = 1000
   keyofvalue = ""
   if name_hash == {}
     return nil
@@ -16,3 +16,12 @@ def key_for_min_value(name_hash)
 keyofvalue
 end
 end
+
+winner = ""
+passengers.each do |suite, name|
+  if suite == :suite_a && name.start_with?("A")
+    winner = name
+  end
+end
+ 
+winner  
