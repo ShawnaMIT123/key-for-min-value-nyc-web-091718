fbe9e856c851443321ddcd11e0d30690a1687928
Shawna Davis <shawna_davis@alum.mit.edu>
2018-09-13 19:06:10 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,8 +7,8 @@ def key_for_min_value(name_hash)
   if name_hash == {}
     return nil
   else
-  name_hash.each { |key, value|
-    if valuemin > value
+  name_hash.each do |key, value|
+    if value <= valuemin
       valuemin = value
       keyofvalue = key
     end
